a0ee107e95359612e29a92e575e0e312ecf6912e
1ung <leewhe@hotmail.com>
2018-04-10 16:47:10 +0800

    completed delv

--- ruby/google_shopping_functions.rb
+++ ruby/google_shopping_functions.rb
@@ -2,10 +2,10 @@ require 'byebug'
 require 'json'
 require 'pp'
 
-file = File.read('products.json')
+file = File.read('../products.json')
 data = JSON.parse(file, {:symbolize_names => true})
 
-pp data
+#pp data
 
 # example function called get_items_count
 # input: accepts full item data
@@ -20,6 +20,41 @@ end
 #byebug
 
 # Define and use your functions here
+# 1.) getItems(objectData)
+def get_items(item_data)
+  item_data[:items]
+end
+
+# 2.) getItemsByBrand(items, brand)
+def get_items_by_brand(items, brand)
+  puts items
+    items.select{|item| item[:product][:brand].downcase == brand.downcase}
+end
+
+# 3.) getItemsByAuthor(items, author)
+def get_items_by_author(items, author)
+  items.select{|item| item[:product][:author][:name].downcase == author.downcase}
+end
+
+# 4.) getAvailableProducts(items)
+def get_available_products(items)
+  items.select{|item| item[:product][:inventories][0][:availability] == 'inStock'}
+end
+
+# 5.) Use your functions
+
+# All items made by Sony.
+pp get_items_by_brand(get_items(data), 'Sony')
+
+# All items made by Sony that are available.
+pp get_available_products(get_items_by_brand(get_items(data), 'Sony'))
+
+# All available items by the author "Adorama Camera"
+pp get_available_products(get_items_by_author(get_items(data), 'Adorama Camera'))
+
+# All items made by Nikon with the author eBay.
+pp get_items_by_author(get_items_by_brand(get_items(data), 'Nikon'), 'eBay')
 
 # output item count using the getItemsCount function
 puts "Item Count: #{get_items_count( data )}"
+
