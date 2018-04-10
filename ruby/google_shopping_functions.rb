22f9843c7a6996b722f6fec41d44d7d6c855ced7
jerrypan90 <jerrypan_90@hotmail.com>
2018-04-10 23:20:03 +0800

    added completed homework

--- ruby/google_shopping_functions.rb
+++ ruby/google_shopping_functions.rb
@@ -2,10 +2,10 @@ require 'byebug'
 require 'json'
 require 'pp'
 
-file = File.read('products.json')
+file = File.read('../products.json')
 data = JSON.parse(file, {:symbolize_names => true})
 
-pp data
+# pp data
 
 # example function called get_items_count
 # input: accepts full item data
@@ -23,3 +23,44 @@ end
 
 # output item count using the getItemsCount function
 puts "Item Count: #{get_items_count( data )}"
+
+#qn1
+def get_items(input)
+  input[:items]
+end
+
+#qn2
+def get_items_by_brand(input, brand)
+  ans = input[:items].select {|item| item[:product][:brand] == brand}
+  ans.map {|ans| ans[:product][:title]}
+end
+# puts get_items_by_brand(data, "Sony")
+
+#qn3
+def get_items_by_author(input, author)
+  ans = input[:items].select {|item| item[:product][:author][:name].include?(author)}
+  ans.map {|ans| ans[:product][:title]}
+end
+# puts get_items_by_author(data, "eBay")
+
+#qn4
+def get_available_products(input)
+  ans = input[:items].select {|item| item[:product][:inventories][0][:availability] == "inStock"}
+  ans.map {|ans| ans[:product][:title]}
+end
+# puts get_available_products(data)
+
+#qn5a
+# puts get_items_by_brand(data, "Sony")
+
+#qn5b
+available_Sony_products = (get_items_by_brand(data, "Sony") & get_available_products(data))
+# puts available_Sony_products
+
+#qn5c
+available_adorama_camera = (get_items_by_author(data, "Adorama Camera") & get_available_products(data))
+# puts available_adorama_camera
+
+#qn5d
+nikon_brand_eBay_author = (get_items_by_brand(data, "Nikon") & get_items_by_author(data, "eBay"))
+# puts nikon_brand_eBay_author
