175ed8f272310094632431446de093c738d2af25
RequiemDK <kok.chee.kean@outlook.com>
2018-04-10 16:06:59 +0800

    because scott said my lazy method was too complicated :(

--- ruby/google_shopping_functions.rb
+++ ruby/google_shopping_functions.rb
@@ -5,7 +5,6 @@ require 'pp'
 file = File.read('../products.json')
 data = JSON.parse(file, {:symbolize_names => true})
 
-$data = data[:items]
 # pp data
 
 # example function called get_items_count
@@ -25,43 +24,28 @@ end
 
 # Define and use your functions here
 def get_items_by_brand(items, brand)
-  if (items.class == Array)
-    data = items
-  else
-    data = items[:items]
-  end
-  data.select{|item| item[:product][:brand].downcase == brand.downcase}
+  items.select{|item| item[:product][:brand].downcase == brand.downcase}
 end
 
 def get_items_by_author(items, author)
-  if (items.class == Array)
-    data = items
-  else
-    data = items[:items]
-  end
-  data.select{|item| item[:product][:author][:name].downcase.include?(author.downcase)}
+  items.select{|item| item[:product][:author][:name].downcase.include?(author.downcase)}
 end
 
 def get_available_products(items)
-  if (items.class == Array)
-    data = items
-  else
-    data = items[:items]
-  end
-  data.select{|item| item[:product][:inventories][0][:availability] == 'inStock'}
+  items.select{|item| item[:product][:inventories][0][:availability] == 'inStock'}
 end
 
 # output item count using the getItemsCount function
 # puts "Item Count: #{get_items_count( data )}"
 
 # puts "Items made by Sony: "
-# pp get_items_by_brand( data, 'Nikon')
+# pp get_items_by_brand( get_items( data ), 'Sony' )
 
 # puts "Items made by Sony that are available: "
-# pp get_available_products(get_items_by_brand(data, 'Sony'))
+# pp get_available_products( get_items_by_brand( get_items( data ), 'Sony' ) )
 
 # puts "Items by Adorama Camera that are available: "
-# pp get_available_products(get_items_by_author(data, 'Adorama Camera'))
+# pp get_available_products( get_items_by_author( get_items( data ), 'Adorama Camera' ) )
 
 # puts "All items made by Nikon with the author eBay: "
-# pp get_items_by_author(get_items_by_brand(data, 'Nikon'), 'eBay')
+# pp get_items_by_author( get_items_by_brand( get_items( data ), 'Nikon' ), 'eBay' )
