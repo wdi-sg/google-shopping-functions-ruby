2632249b616a67252d910298d167ad0560ae76aa
Phangster <mynamebryanphang@gmail.com>
2018-07-15 10:29:44 +0800

    Completed

--- ruby/google_shopping_functions.rb
+++ ruby/google_shopping_functions.rb
@@ -5,21 +5,57 @@ require 'pp'
 file = File.read('products.json')
 data = JSON.parse(file, {:symbolize_names => true})
 
-pp data
+#$data = data[:items]
+#pp data
 
 # example function called get_items_count
 # input: accepts full item data
 # output: returns the length of the items array
 
-def get_items_count( item_data )
-
-  item_data.size
-end
-
 # use byebug to set a breakpoint
 #byebug
 
-# Define and use your functions here
+# Checking functions
 
-# output item count using the getItemsCount function
-puts "Item Count: #{get_items_count( data )}"
+def get_items( item_data )
+	item_data[:items]
+end
+
+def avail_items(items)
+	items.select{|i| i[:product][:inventories][0][:availability] == "inStock"}.length
+end
+
+# Operating functions
+
+def get_items_count( items, brand )
+	result =items.select{|i| i[:product][:brand] == brand}.length
+end
+
+def get_items_by_brand(items,brand)
+	result = items.select{|i| i[:product][:brand] == brand}
+end
+
+def get_items_by_author(items,author)
+	result = items.select{ |i| i[:product][:author][:name] == author }
+end
+ 
+# Output
+
+puts "Number of Sony items: #{get_items_count(get_items(data), 'Sony')}"
+
+puts "Items made by Sony: #{get_items_by_brand(get_items(data),'Sony')}"
+
+puts "Available items made by Sony: #{avail_items(get_items_by_brand(get_items(data),'Sony'))}"
+
+puts "By author: #{avail_items(get_items_by_author(get_items(data), 'Adorama Camera') )}"
+
+puts "Items made by Nikon with the author eBay: #{get_items_by_author(get_items_by_brand(get_items(data), 'Nikon'), 'eBay')}"
+
+
+
+
+
+
+
+
+
