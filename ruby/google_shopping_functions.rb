bdfd6226ebbcca5c243bc046b96d746371856bb6
tingsoon <angts8@hotmail.com>
2018-07-16 08:35:04 +0800

    completed hw.

--- ruby/google_shopping_functions.rb
+++ ruby/google_shopping_functions.rb
@@ -1,8 +1,8 @@
-require 'byebug'
+# require 'byebug'
 require 'json'
 require 'pp'
 
-file = File.read('products.json')
+file = File.read('../products.json')
 data = JSON.parse(file, {:symbolize_names => true})
 
 pp data
@@ -12,7 +12,6 @@ pp data
 # output: returns the length of the items array
 
 def get_items_count( item_data )
-
   item_data.size
 end
 
@@ -21,5 +20,30 @@ end
 
 # Define and use your functions here
 
+def get_items( item_data )
+  item_data[:items]
+end
+
+def get_brands ( items, brand )
+  result = items.select { |i| i[:product][:brand] == brand }
+end
+
+def get_author ( items, author )
+  result = items.select { |i| i[:product][:author][:name] == author }
+end
+
+def get_available_products (items)
+  result = items.select { |i| i[:product][:inventories][0][:availability] == 'inStock' }
+end
+
 # output item count using the getItemsCount function
-puts "Item Count: #{get_items_count( data )}"
+# puts "Item Count: #{get_items_count( data )}"
+# puts "Items #{get_items( data )}"
+# puts "#{get_brands(get_items(data), 'Sony')}"
+# puts "#{get_author(get_items(data), 'eBay')}"
+
+
+puts "1) #{get_brands(get_items(data), 'Sony')}"
+puts "2) #{get_available_products(get_brands(get_items(data), 'Sony'))}"
+puts "Third Q: #{get_available_products(get_author(get_items(data), 'Adorama Camera') )}"
+puts "Fourth Q: #{get_author(get_brands(get_items(data), 'Nikon'), 'eBay' )}"
