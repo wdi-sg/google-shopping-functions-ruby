b68d65313fe5ebf9831669aa1a19b21c8fcab4b4
acechua <acecyq@gmail.com>
2018-07-16 09:04:56 +0800

    try further. cannot seem to get .each to work in a block

--- ruby/google_shopping_functions.rb
+++ ruby/google_shopping_functions.rb
@@ -79,8 +79,8 @@ end
 
 # puts "1. Sony items: #{getItemsByBrand(items, 'Sony')}"
 # puts "2. Available Sony items: #{getItemsByBrand(getAvailableProducts(items), 'Sony')}"
-# puts "3. Available items by author Adorama Camera: #{getItemsByAuthor(getAvailableProducts(items), 'Adorama Camera')}
-# puts "4. Nikon items by author eBay: #{getItemsByAuthor(getItemsByBrand(items, 'Nikon'), 'eBay')}
+# puts "3. Available items by author Adorama Camera: #{getItemsByAuthor(getAvailableProducts(items), 'Adorama Camera')}"
+# puts "4. Nikon items by author eBay: #{getItemsByAuthor(getItemsByBrand(items, 'Nikon'), 'eBay')}"
 
 
 
@@ -91,12 +91,67 @@ end
 
 # puts "Any Lytro product?: #{items.any? {|item| item[:product][:brand] == 'Lytro'}}"
 
-items.each_with_index { |val, index| puts "#{index}. #{val[:product][:title]}" }
+# items.each_with_index { |val, index| puts "#{index}. #{val[:product][:title]}" }
+# getItemsByBrand(items, 'Nikon').each_with_index { |item, index| puts "#{index}. #{item[:product][:title]}" }
 
+# pentax =  items.find { |item| item[:product][:brand] == 'Pentax' }
+# puts "One of the Pentax products: #{pentax[:product][:title]}"
+# expItem = items.find { |item| item[:product][:inventories][0][:price] > 2000 }
+# puts "One of the products above $2000: #{expItem[:product][:title]}"
 
+=begin
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
 
+NOT WORKING BELOW
 
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
+=end
 
+# all_pentax = items.find_all { |item| item[:product][:brand] == 'Pentax' }
+# puts "All Pentax products: #{all_pentax}"
+# all_paid_shipping = items.find_all { |item| (item[:product][:inventories][0][:shipping]).to_i > 0 }
+# puts "All products with paid shipping: #{all_paid_shipping}"
 
+# puts "Massive item index: #{items.find_index { |item| item[:product][:description].include? "Massive" }}"
+# puts "Backorder item index: #{items.find_index { |item| item[:product][:inventories][0][:availability] == "backorder" }}"
+
+# puts "Items list: #{items.map { |item| item[:product][:title] }}"
+# puts "Price list: #{items.map { |item| item[:product][:inventories][0][:price] }}"
+
+# longest = items.reduce do |item_1, item_2|
+# 	item_1[:product][:title].length > item_2[:product][:title].length ? item_1 : item_2
+# end
+# puts "Longest title: #{longest[:product][:title]}"
+# most_expensive = items.reduce do |item_1, item_2|
+# 	(item_1[:product][:inventories][0][:price]).to_f > (item_2[:product][:inventories][0][:price]).to_f ? item_1 : item_2
+# end
+# puts "Most expensive item: #{most_expensive[:product][:title]}"
+
+=begin
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
+
+NOT WORKING BELOW
+
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++
+=end
+
+# puts "Non-Nikon items: #{items.reject { |item| item[:product][:brand] == 'Nikon' }}"
+# puts "Old items: #{items.reject { |item| item[:product][:condition] == "new" }}"
+
+# puts "Nikkon items: #{items.select { |item| item[:product][:brand] == 'Nikon' }}"
+
+# puts "Least to most expensive items: #{items.sort_by { |item| (item[:product][:inventories][0][:price]).to_f }}"
+
+# puts "Nikon product: #{items.take_while { |item| item[:product][:brand] == 'Nikon' }}"
+
+# puts "Unique brands: #{items.uniq { |item| item[:product][:brand] }}"
 
 
